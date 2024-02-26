e204e089be1c7ef3dda32d271ac1db5b34532339
vamseekrishnamedasani <vamsee.k1993@gmail.com>
2024-02-26 19:45:46 +0530

    updated new code

---
+++ src/prctis/Stringsss.java
@@ -0,0 +1,17 @@
+package prctis;
+
+public class Stringsss {
+
+	public static void main(String[] args) {
+
+
+		
+		String str1= "Krishna";
+		
+		String str2 = new String("VAmsee");
+		
+		
+		System.out.println(str1);
+	}
+
+}
